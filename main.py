172d71243e9853106e0a71beb16f3842147455a5
yaniv <yaniv.ayalon@gmail.com>
2021-10-05 21:26:22 +0300

    commit no.1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/0')
 def home():
     return render_template('theme-clean.html')
+
+@app.route('/getPlotCSV') # this is a job for GET, not POST
+def plot_csv():
+    return send_file('yaniv.csv',
+                     mimetype='text/csv',
+                     attachment_filename='yaniv.csv',
+                     as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
