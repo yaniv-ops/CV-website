7e7f60ecc9d9312cfb3f5e4a0cb432fa6ad0d857
yaniv <yaniv.ayalon@gmail.com>
2021-10-09 13:27:57 +0300

    commit no.1

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from flask import Flask, render_template, send_file
+import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators
+from wtforms.validators import DataRequired, Email
+from wtforms.fields.html5 import EmailField
+
+url = "https://api.npoint.io/262095a444176df0c93e"
+
+response = requests.get(url=url)
+db = response.json()
+
+
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "MY_SECRET"
 
-@app.route('/')
-@app.route('/0')
+msg_sent = 'msg has sent'
+class NamerForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    e_mail = EmailField('Email', validators=[DataRequired(), Email()])
+    message = StringField('Write your message here', validators=[DataRequired()])
+    submit = SubmitField('Send message')
+
+@app.route('/', methods=["GET", "POST"])
+@app.route('/0', methods=["GET","POST"])
 def home():
-    return render_template('theme-clean.html')
+    form = NamerForm()
+    if form.validate_on_submit():
+        form.submit.label = 'msg_sent'
+    return render_template('theme-clean.html', post=db, form=form)
 
 @app.route('/getPlotCSV') # this is a job for GET, not POST
 def plot_csv():
@@ -13,6 +37,10 @@ def plot_csv():
                      mimetype='text/csv',
                      attachment_filename='yaniv.csv',
                      as_attachment=True)
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
